3d53dfa61105c9a48d44904e07385e8edabb8aa6
Yaroslav Badyora <yaroslav.badyora@growthkitchen.co>
2025-01-12 17:48:02 +0200

    main - degrade to express

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import { Request, Response, NextFunction } from 'express'
+import * as bodyParser from 'body-parser'
+import router from './router'
+
+export const app = express()
+
+// Custom error handler
+function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+  console.error(err.stack)
+  res.status(500).json({ error: err.message })
+}
+
+// Default route
+function defaultRoute(req: Request, res: Response, next: NextFunction) {
+  res.sendStatus(404)
+}
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/', router)
+app.use(defaultRoute) // default route has to be last route
+app.use(errorHandler) // Error handler goes last
---
+++ src/router.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.get('/health', function(req, res) {
+  res.status(200).send("OK")
+})
+router.get('/ping', function(req, res) {
+  res.status(200).send("pong")
+})
+
+export default router
